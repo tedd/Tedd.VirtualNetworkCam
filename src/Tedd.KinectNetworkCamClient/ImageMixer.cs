47365e8d4f03d3b48c58f24c8f43951590a76c07
Tedd Hansen <tedd@konge.net>
2020-05-22 20:51:40 +0200

    Refactoring WPF app to make code clearer

---
+++ src/Tedd.KinectNetworkCamClient/ImageMixer.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using Tedd.KinectNetworkCamClient.Annotations;
+using Tedd.RandomUtils;
+
+namespace Tedd.KinectNetworkCamClient
+{
+    internal class ImageMixer : INotifyPropertyChanged
+    {
+        public int Width { get; }
+        public int Height { get; }
+        public Tedd.WriteableBitmap BackgroundImage;
+        public Tedd.WriteableBitmap ForegroundImage;
+        public Tedd.WriteableBitmap FinishedImage;
+        public readonly ReaderWriterLockSlim UpdateLock = new ReaderWriterLockSlim(LockRecursionPolicy.NoRecursion);
+        private readonly FastRandom _fr = new FastRandom();
+
+        public ImageSource BackgroundImageSource
+        {
+            get => BackgroundImage.BitmapSource;
+        }
+
+        public ImageSource ForegroundImageSource
+        {
+            get => ForegroundImage.BitmapSource;
+        }
+
+        public ImageSource FinishedImageSource
+        {
+            get => FinishedImage.BitmapSource;
+        }
+
+        public ImageMixer(int width, int height)
+        {
+            Width = width;
+            Height = height;
+            BackgroundImage = new WriteableBitmap(width, height, PixelFormats.Bgra32);
+            ForegroundImage = new WriteableBitmap(width, height, PixelFormats.Bgra32);
+            FinishedImage = new WriteableBitmap(width, height, PixelFormats.Bgra32);
+        }
+
+        public unsafe void RepaintFinishedImage()
+        {
+            UpdateLock.EnterWriteLock();
+            try
+            {
+                var size = Width * Height;
+
+                var background = new Span<byte>((void*) BackgroundImage.MapView.ToPointer(), size * 4);
+                var cameraUInt = new Span<UInt32>((void*) ForegroundImage.MapView.ToPointer(), size * 4);
+                var finishedUInt = new Span<UInt32>((void*) FinishedImage.MapView.ToPointer(), size * 4);
+
+                // Blend
+                for (int i = 0; i < size; i++)
+                {
+                    var src = cameraUInt[i];
+                    finishedUInt[i] = Blend(background[i], src, (byte) src);
+                }
+            }
+            finally
+            {
+                UpdateLock.ExitWriteLock();
+            }
+        }
+
+        private static UInt32 Blend(UInt32 color1, UInt32 color2, UInt32 alpha)
+        {
+            UInt32 rb = color1 & 0xff00ff;
+            UInt32 g = color1 & 0x00ff00;
+            rb += ((color2 & 0xff00ff) - rb) * alpha >> 8;
+            g += ((color2 & 0x00ff00) - g) * alpha >> 8;
+            return (rb & 0xff00ff) | (g & 0xff00);
+        }
+
+        public unsafe void CopyTo(Span<byte> target, int width, int height, int bytesPerPixel)
+        {
+            try
+            {
+                UpdateLock.EnterReadLock();
+                
+                // Source image
+                var image = FinishedImage.ToSpanByte();
+
+                // Copy image to target. Target may not match size or bit depth.
+                // So... First of all, lets not resize image. We copy whatever we have.
+                // If target cam is too large then surrounding area will be black
+                // If target cam is too small then image will be cropped.
+                var iWidth = Math.Min(Width, width);
+                var iHeight = Math.Min(Height, height);
+                var dcl = target.Length - 1;
+                for (var x = 0; x < iWidth; x++)
+                {
+                    for (var y = 0; y < iHeight; y++)
+                    {
+                        var si = ((y * Width) + x) * 4;
+
+                        var sd = ((y * width) + x) * bytesPerPixel;
+                        // Flip y axis
+                        //var sd = ((y * _client._width) + (_client._width-1-x)) * _client._bytesPerPixel;
+
+                        // 24-bit
+                        if (bytesPerPixel == 3)
+                        {
+                            target[dcl - (sd + 0)] = image[si + 2];
+                            target[dcl - (sd + 1)] = image[si + 1];
+                            target[dcl - (sd + 2)] = image[si + 0];
+                        }
+                        // 32-bit
+                        else if (bytesPerPixel == 4)
+                        {
+                            target[dcl - (sd + 0)] = image[si + 4];
+                            target[dcl - (sd + 1)] = image[si + 3];
+                            target[dcl - (sd + 2)] = image[si + 2];
+                            target[dcl - (sd + 4)] = image[si + 1];
+                        }
+                        else
+                        {
+                            // If we do not recognize BytesPerPixel we randomize data so its easier to debug. This will be static.
+                            for (var i = 0; i < bytesPerPixel; i++)
+                                target[sd + i] = _fr.NextByte();
+                        }
+                    }
+                }
+
+            }
+            finally
+            {
+                UpdateLock.ExitReadLock();
+            }
+        }
+
+
+        #region INotifyPropertyChanged
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
+
+        public void UpdateForeground(Span<byte> image)
+        {
+            UpdageImage(ForegroundImage, image);
+        }
+
+        public void UpdateBackground(Span<byte> image)
+        {
+            UpdageImage(BackgroundImage, image);
+        }
+
+        private unsafe void UpdageImage(WriteableBitmap target, Span<byte> image)
+        {
+            try
+            {
+                UpdateLock.EnterWriteLock();
+
+                // Copy image to target
+                image.CopyTo(target.ToSpanByte());
+            }
+            finally
+            {
+                UpdateLock.ExitWriteLock();
+            }
+        }
+    }
+}
--- src/Tedd.KinectNetworkCamClient/MainWindow.xaml.cs
+++ src/Tedd.KinectNetworkCamClient/MainWindow.xaml.cs
@@ -28,19 +28,12 @@ namespace Tedd.KinectNetworkCamClient
     /// </summary>
     public partial class MainWindow : Window
     {
-        private WriteableBitmap _bitmap = null;
         private KinectCam _cam;
         private NetworkCamDriverClient _client;
         private Thread _sendThread;
         private readonly ManualResetEventSlim _nextImageWaiter=new ManualResetEventSlim(false);
+        private ImageMixer _imageMixer;
 
-        public ImageSource ImageSource
-        {
-            get
-            {
-                return _bitmap;
-            }
-        }
         /// <summary>
         /// Initializes a new instance of the MainWindow class.
         /// </summary>
@@ -48,120 +41,62 @@ namespace Tedd.KinectNetworkCamClient
         {
             this.InitializeComponent();
 
-            DataContext = this;
-            _cam = new KinectCam();
-            _bitmap = new WriteableBitmap(_cam.ColorFrameDescription.Width, _cam.ColorFrameDescription.Height, 75, 75, PixelFormats.Bgra32, null);
-            //_bitmapMem= new Memory<byte>((void*)_bitmap.BackBuffer, _bitmap.PixelWidth * _bitmap.PixelHeight * 4);
-
-            _cam.NewImage += CamOnNewImage;
             
+            _cam = new KinectCam();
+            _cam.NewImage += CamOnNewImage;
+
+            _imageMixer = new ImageMixer(_cam.ColorFrameDescription.Width, _cam.ColorFrameDescription.Height);
+            // TODO: Load background PNG file into BackgroundImage
+
+            DataContext = _imageMixer;
+
             _client = new NetworkCamDriverClient();
             _client.Connect("127.0.0.1", 9090);
+            // TODO: Not best way to connect :)
             Thread.Sleep(1000);
+
+            // Start a separate thread for sending image
             _sendThread = new Thread(SendThreadLoop) { Name = "SendThread", IsBackground = true };
             _sendThread.Start();
         }
-        private FastRandom fr = new FastRandom();
 
-        private unsafe void SendThreadLoop()
+        private void SendThreadLoop()
         {
-            byte[] backBuffer = null;
-            Span<byte> backBufferSpan = null;
+            var backBuffer = new byte[_client.Width * _client.Height * _client.BytesPerPixel];
+            var backBufferSpan = (Span<byte>)backBuffer;
             for (; ; )
             {
+                // Wait for next image to be ready
+                _nextImageWaiter.Reset();
                 _nextImageWaiter.Wait();
-                _cam.ColorImageLock.EnterReadLock();
 
-                var image = new ReadOnlySpan<byte>(_cam.ColorImage);
-                //var image = st;
-                var cl = _client.Width * _client.Height * _client.BytesPerPixel;
-                if (backBuffer == null || backBuffer.Length != cl)
-                {
-                    backBuffer = new byte[cl];
-                    backBufferSpan = new Span<byte>(backBuffer);
-                }
-
-                //image.CopyTo(backBufferSpan);
-
-                var width = Math.Min(_cam.ColorFrameDescription.Width, _client.Width);
-                var height = Math.Min(_cam.ColorFrameDescription.Height, _client.Height);
-                var dcl = backBufferSpan.Length -1;
-                for (var x = 0; x < width; x++)
-                {
-                    for (var y = 0; y < height; y++)
-                    {
-                        var si = ((y * _cam.ColorFrameDescription.Width) + x) * 4;
-
-                        var sd = ((y * _client.Width) + x) * _client.BytesPerPixel;
-                        // Flip y axis
-                        //var sd = ((y * _client._width) + (_client._width-1-x)) * _client._bytesPerPixel;
-
-                        if (_client.BytesPerPixel == 3)
-                        {
-                            backBufferSpan[dcl - (sd + 0)] = image[si + 2];
-                            backBufferSpan[dcl - (sd + 1)] = image[si + 1];
-                            backBufferSpan[dcl - (sd + 2)] = image[si + 0];
-                        }
-                        else if (_client.BytesPerPixel == 4)
-                        {
-                            backBufferSpan[dcl - (sd + 0)] = image[si + 4];
-                            backBufferSpan[dcl - (sd + 1)] = image[si + 3];
-                            backBufferSpan[dcl - (sd + 2)] = image[si + 2];
-                            backBufferSpan[dcl - (sd + 4)] = image[si + 1];
-                        }
-                        else
-                        {
-                            for (var i = 0; i < _client.BytesPerPixel; i++)
-                                backBufferSpan[sd + i] = fr.NextByte();
-                        }
-                    }
-                }
-                _cam.ColorImageLock.ExitReadLock();
-
-                ////MemoryExtensions.Reverse(ispan);
-                //for (var i = 0; i < ispan.Length; i++)
-                //{
-                //    if (ispan[i] == 0)
-                //        ispan[i] = fr.NextByte();
-                //}
-                //Thread.Sleep(10);
+                // Copy (convert) mixed image to backbuffer
+                _imageMixer.CopyTo(backBufferSpan, _client.Width, _client.Height, _client.BytesPerPixel);
+                
+                // Send backbuffer
                 _client.SendImage(backBuffer);
-                //_client.SendImage(new ReadOnlySpan<byte>(_cam.ColorImage));
             }
-
         }
 
-        private unsafe void CamOnNewImage(KinectCam sender, byte[] image)
+        private void CamOnNewImage(KinectCam sender, byte[] image)
         {
+            // Copy to mixer
+            _imageMixer.UpdateForeground(image);
+            _imageMixer.RepaintFinishedImage();
+            InvalidateImage(_imageMixer.ForegroundImage);
+            InvalidateImage(_imageMixer.FinishedImage);
+
+            // Trigger new copy to backbuffer and send
             _nextImageWaiter.Set();
+        }
 
-            _bitmap.Lock();
-            var sp = new Span<byte>(image);
-            var st = new Span<byte>((void*)_bitmap.BackBuffer, _bitmap.PixelWidth * _bitmap.PixelHeight * 4);
-            _cam.ColorImageLock.EnterReadLock();
-            sp.CopyTo(st);
-            _cam.ColorImageLock.ExitReadLock();
-            _nextImageWaiter.Reset();
-
-            //for (var x = 0; x < 1920; x++)
-            //{
-            //    for (var y = 0; y < 816; y++)
-            //    {
-            //        var si = ((y * 1920) + x) * 4;
-            //        var sd = ((y * 1920) + x) * 4;
-            //        st[sd + 0] = image[si + 0];
-            //        st[sd + 1] = image[si + 1];
-            //        st[sd + 2] = image[si + 2];
-            //    }
-            //}
-
-            _bitmap.AddDirtyRect(new Int32Rect(0, 0, _bitmap.PixelWidth, _bitmap.PixelHeight));
-            _bitmap.Unlock();
-
-            
-
-            //_continueWaiter.WaitOne();
-
+        private void InvalidateImage(WriteableBitmap writeableBitmap)
+        {
+            // We can only invalidate from GUI thread
+            if (Dispatcher.CheckAccess())
+                writeableBitmap.Invalidate();
+            else
+                Dispatcher.Invoke(writeableBitmap.Invalidate);
         }
     }
 }
